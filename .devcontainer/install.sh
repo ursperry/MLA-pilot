d3f046c121803c0efe131f3d2315dbc484a1c3b4
nadinedelia <nadine.delia@yahoo.com>
2023-11-07 10:46:39 +0800

    startup refactor - check-installations run at container startup

--- .devcontainer/install.sh
+++
@@ -1,5 +0,0 @@
-wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | sudo apt-key add - \
-    && echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list \
-    && sudo apt-get update \
-    && sudo apt-get install -y mongodb-mongosh mongodb-org-server \
-    && sudo apt-get clean
